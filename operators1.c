0159686783511236c6cd49d0a7efc3df4ce15898
HARSH GUPTA <harshgupta@MacBook-Air.local>
2023-03-09 19:41:52 +0530

    Added size1.c file

---
+++ operators1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    float b;
+    a=45;
+    b=64.82;
+    printf("a+b=%f\n",a+b);
+    printf("a-b=%f\n",a-b);
+    printf("a*b=%f\n",a*b);
+    printf("a/b=%f\n",a/b);  
+
+} 
+
+in case of float use %f,in case of int use %d
