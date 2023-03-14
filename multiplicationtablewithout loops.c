1c6e7296cb96b2073527fa7ee68d8b1d9a9b8dd1
HARSH GUPTA <harshgupta@MacBook-Air.local>
2023-03-14 11:50:18 +0530

    Added multiplication table without loops.c file

---
+++ multiplicationtablewithout loops.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d,e,f,g,h,i,j,k;
+    a=4;
+    b=1;
+    c=2;
+    d=3;
+    e=4;
+    f=5;
+    g=6;
+    h=7;
+    i=8;
+    j=9;
+    k=10;
+    printf("a*b=%d\n",a*b);
+    printf("a*c=%d\n",a*c);
+    printf("a*d=%d\n",a*d);
+    printf("a*e=%d\n",a*e);
+    printf("a*f=%d\n",a*f);
+    printf("a*g=%d\n",a*g);
+    printf("a*h=%d\n",a*h);
+    printf("a*i=%d\n",a*i);
+    printf("a*j=%d\n",a*j);
+    printf("a*k=%d\n",a*k);
+}
+
+
+
+
+
+
+
+
+
+
