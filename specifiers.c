7839a3328d05e763c7b9e44e8c76e7a46444baeb
HARSH GUPTA <harshgupta@MacBook-Air.local>
2023-03-14 12:19:59 +0530

    Added specifiers.c file

---
+++ specifiers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    float b;
+    a=7;
+    b=7.34;
+    printf("The value of a is %d and the value of b is %8.4f\n",a,b);
+    return 0;
+}
+/* 8.4 means 8 character ki space mein 4 decimal places tak result chaiye */
