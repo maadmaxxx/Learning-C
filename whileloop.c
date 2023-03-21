0f6dc95025026549d695bc315d937467be9066fd
HARSH GUPTA <harshgupta@MacBook-Air.local>
2023-03-21 21:33:47 +0530

    Added whileloop.c file

---
+++ whileloop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter an integer\n");
+    scanf("%d",&n);
+while (n !=0)
+{ printf("Enter an integer\n");
+    scanf("%d",&n);
+}
+printf("You are oout of the loop");
+}    
